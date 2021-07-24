84f0b666544bda9fbbfce34860458c9918c008a9
Ariel Lee <aersachi@gmail.com>
2021-07-23 20:46:44 -0700

    fixed UI issue where price data isn't available. removed rating, star, and review count

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,35 +1,18 @@
 import React, { Component } from 'react';
 
-const star = String.fromCharCode(9733);
 const dot = String.fromCharCode(8901);
 
 export class Business extends Component {
   constructor(props) {
     super(props);
   }
-  starRating(rating) {
-    switch(rating) {
-      case 1:
-          return <span>{star}</span>;
-      case 1.5:
-          return <span>{star}<span className="halfStar">{star}</span></span>;
-      case 2:
-          return <span>{star + star}</span>;
-      case 2.5:
-          return <span>{star + star}<span className="halfStar">{star}</span></span>;
-      case 3:
-          return <span>{star + star + star}</span>;
-      case 3.5:
-          return <span>{star + star + star}<span className="halfStar">{star}</span></span>;
-      case 4:
-          return <span>{star + star + star + star}</span>;
-      case 4.5:
-          return <span>{star + star + star + star}<span className="halfStar">{star}</span></span>;
-      case 5:
-          return <span>{star + star + star + star + star}</span>;
-      default:
-          return <span>Rating unavailable</span>;
+
+  priceToString(price) {
+    console.log("!!!! PRICE: " + price);
+    if(price == undefined) { //If price data isn't available, don't show anything
+      return;
     }
+    return(" " + dot + " " + price);
   }
 
   render() {
@@ -44,7 +27,7 @@ export class Business extends Component {
               <h1 className="business-name"><a href={ this.props.business.url } target="_blank" >{ this.props.business.name }</a></h1>
               <p className="details">
                 <span>
-                  { this.props.business.rating } { this.starRating(this.props.business.rating) } ({ this.props.business.reviewCount }) { " " + dot + " " + this.props.business.category[0].title + " " + dot + " " + this.props.business.price}
+                  { this.props.business.category[0].title} { this.priceToString(this.props.business.price) } 
                 </span>
               </p>
               <p>{ this.props.business.address } <br />
