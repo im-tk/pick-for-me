8be3a294892a83a498052089f1f1835252382945
Ariel Lee <aersachi@gmail.com>
2021-07-25 23:13:18 -0700

    imported fonts and cleaned Header and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className="footer">
+      <div className="footer-container">
           <span>created by <a href="http://github.com/im-tk" target="_window">@im-tk</a></span>
       </div>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,22 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-        <h1 id="app-title">{this.props.children}</h1>
+        <div className="header-container">
+          <h1>{this.props.children}</h1>
+        </div>
     );
   }
 };
 
 export default Header;
+
+/**
+ * 
+
+      <div className="footer">
+          <span>created by <a href="http://github.com/im-tk" target="_window">@im-tk</a></span>
+      </div>
+
+ * 
+ * 
+ */
