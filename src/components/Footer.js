77f24a329245dae80a4bce4bb1058344cb788c98
Ariel Lee <aersachi@gmail.com>
2021-08-09 18:18:40 -0700

    added link to github repository in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,13 @@ class Footer extends Component {
   render() {
     return (
       <div className="footer-container">
-          <span><b>pick for me</b> created by <a href="http://github.com/im-tk" target="_window"><b>@im-tk</b></a></span>
+          <span>
+            <a href="https://github.com/im-tk/pick-for-me" target="_blank" rel="noopener noreferrer"><b>pick for me</b></a>
+            {' '}
+            created by 
+            {' '}
+            <a href="http://github.com/im-tk" target="_blank" rel="noopener noreferrer"><b>@im-tk</b></a>
+          </span>
       </div>
     );
   }
