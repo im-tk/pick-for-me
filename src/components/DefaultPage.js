78ea663c98e30b41f069423237b8fe53b711314b
Ariel Lee <aersachi@gmail.com>
2021-08-15 13:19:51 -0700

    styles for DefaultPage added

--- src/components/DefaultPage.js
+++ src/components/DefaultPage.js
@@ -1,7 +1,7 @@
 function DefaultPage() {
   return(
     <div className="default-container">
-      <h1 className="default-message">Please allow access to your geolocation to continue.</h1>
+      <span className="default-message">Please allow access to your geolocation to continue.</span>
     </div>
   );
 }
