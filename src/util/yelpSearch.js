54763a2e8f897ba3c0af230e5c53a451ed727e44
aer <aersachi@gmail.com>
2022-10-08 17:54:45 -0700

    added cors url back to baseURL and added return

--- src/util/yelpSearch.js
+++ src/util/yelpSearch.js
@@ -1,10 +1,8 @@
 import getDistanceBetween from "./getDistanceBetween";
 const axios = require("axios");
 
-// https://cors-anywhere.herokuapp.com/
-
 const baseURL =
-  "https://api.yelp.com/v3/businesses/search?term=food&radius=16000&open_now=true&limit=50";
+  "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=food&radius=16000&open_now=true&limit=50";
 
 let config = {
   method: "get",
@@ -14,22 +12,17 @@ let config = {
   },
 };
 
-// axios version
 async function yelpSearch(latitude, longitude) {
   config.url = baseURL + "&latitude=" + latitude + "&longitude=" + longitude;
-  console.log(config.url);
-  axios(config)
-    .then((response) => {
-      // console.log(response.data);
-      return response.data;
-    })
+  return axios(config)
     .then((response) => {
       return response.data;
     })
-    .then((jsonResponse) => {
-      if (jsonResponse.businesses) {
-        return jsonResponse.businesses.map((business) => {
-          console.log(JSON.stringify(jsonResponse));
+    .then((data) => {
+      console.log("data", data);
+      if (data.businesses) {
+        console.log("hey");
+        return data.businesses.map((business) => {
           return {
             id: business.id,
             imageSRC: business.image_url,
@@ -55,45 +48,5 @@ async function yelpSearch(latitude, longitude) {
     })
     .catch((error) => console.log(error));
 }
-
-// fetch version
-// async function yelpSearch(latitude, longitude) {
-//   return fetch(`${baseURL}&latitude=${latitude}&longitude=${longitude}`, {
-//     headers: {
-//       Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
-//     },
-//   })
-//     .then((response) => {
-//       return response.json();
-//     })
-//     .then((jsonResponse) => {
-//       if (jsonResponse.businesses) {
-//         return jsonResponse.businesses.map((business) => {
-//           console.log(JSON.stringify(jsonResponse));
-//           return {
-//             id: business.id,
-//             imageSRC: business.image_url,
-//             name: business.name,
-//             address: business.location.address1,
-//             city: business.location.city,
-//             state: business.location.state,
-//             zipCode: business.location.zip_code,
-//             category: business.categories,
-//             coordinates: business.coordinates,
-//             phone: business.display_phone,
-//             url: business.url,
-//             price: business.price,
-//             distance: getDistanceBetween(
-//               latitude,
-//               longitude,
-//               business.coordinates.latitude,
-//               business.coordinates.longitude
-//             ),
-//           };
-//         });
-//       }
-//     })
-//     .catch((error) => console.log(error));
-// }
 
 export default yelpSearch;
